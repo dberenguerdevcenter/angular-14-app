267b9f9b2e7e1dd04a70bc7271853711e49b9752
fran.aznar <francisco.aznar@nexoqa.com>
2022-10-17 10:54:30 +0200

    fix

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8081/api/tutorials';
+const baseUrl = 'http://localhost:8082/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
