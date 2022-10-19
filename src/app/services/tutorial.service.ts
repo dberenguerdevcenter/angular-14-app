4cf92978d086bafbc10847416806dea29b3ef0b4
fran.aznar <francisco.aznar@nexoqa.com>
2022-10-19 13:50:10 +0200

    fix backend url

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://spring-boot-app.default:8082/api/tutorials';
+const baseUrl = 'http://10.109.18.93:8082/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
